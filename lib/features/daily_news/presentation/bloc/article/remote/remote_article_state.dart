e4b4ca35d182362fe7de30038d5411f52f24eb22
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-01-25 17:46:46 +0300

    Create Bloc

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -0,0 +1,25 @@
+import 'package:architecture_template/features/daily_news/domain/entities/article.dart';
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class RemoteArticlesState extends Equatable {
+  final List<ArticleEntity>? articles;
+  final DioError? error;
+
+  const RemoteArticlesState({this.articles, this.error});
+
+  @override
+  List<Object> get props => [articles!, error!];
+}
+
+class RemoteArticlesLoading extends RemoteArticlesState {
+  const RemoteArticlesLoading();
+}
+
+class RemoteArticlesDone extends RemoteArticlesState {
+  const RemoteArticlesDone(List<ArticleEntity> article) : super(articles: article);
+}
+
+class RemoteArticlesError extends RemoteArticlesState {
+  const RemoteArticlesError(DioError error) : super(error: error);
+}
