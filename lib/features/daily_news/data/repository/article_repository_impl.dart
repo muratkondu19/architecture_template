b66e86eb98a996b0a8fdc11dd976881908fb586c
Murat Kondu <murat.kondu.mk@hotmail.com>
2024-01-25 17:41:50 +0300

    Use Cases

--- lib/features/daily_news/data/repository/article_repository_impl.dart
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -1,11 +1,37 @@
+import 'dart:io';
+
 import 'package:architecture_template/core/resources/data_state.dart';
+import 'package:architecture_template/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:architecture_template/features/daily_news/data/models/article.dart';
 import 'package:architecture_template/features/daily_news/domain/respository/article_repository.dart';
+import 'package:dio/dio.dart';
+
+import '../../../../core/constants/constants.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
+  final NewsApiService _newsApiService;
+
+  ArticleRepositoryImpl(this._newsApiService);
   @override
-  Future<DataState<List<ArticleModel>>> getNewsArticles() {
-    // TODO: implement getNewsArticles
-    throw UnimplementedError();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() async {
+    try {
+      final httpResponse = await _newsApiService.getNewsArticles(
+        apiKey: newsAPIKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioError(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioErrorType.badResponse,
+            requestOptions: httpResponse.response.requestOptions));
+      }
+    } on DioError catch (e) {
+      return DataFailed(e);
+    }
   }
 }
